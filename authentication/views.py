e2d6074398da2b6ee1aec43b63a8c097c6b328f4
dreins <davynreinhard@gmail.com>
2023-05-05 22:18:35 +0700

    feat: register href & bg

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 def user_login(request):
+    nama = request.POST.get('nama')
+    email = request.POST.get('email')
     return render(request, 'login.html')
 
 def user_register(request):
     return render(request, 'register.html')
+
