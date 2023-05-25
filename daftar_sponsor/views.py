92b0916813ecc15159e179e2c8e09dc8440c62f0
febidamanik <febidamanik@gmail.com>
2023-05-25 22:47:40 +0700

    fixed some error

--- daftar_sponsor/views.py
+++ daftar_sponsor/views.py
@@ -3,11 +3,12 @@ from datetime import datetime
 from daftar_sponsor.query import *
 from django.shortcuts import *
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def show_daftar_sponsor(request):
-    nama_atlet = request.session['user']['nama']
-    pilihan_sponsor_tersedia = pilihan_sponsor(nama_atlet)
+    pilihan_sponsor_tersedia = pilihan_sponsor(request.session['user']['nama'])
 
     if request.method == 'POST':
         nama_brand = request.POST['sponsor']
@@ -15,7 +16,7 @@ def show_daftar_sponsor(request):
         tgl_selesai = request.POST['selesai']
 
         if (nama_brand and tgl_mulai and tgl_selesai):
-            daftar_sponsor(nama_atlet, nama_brand, tgl_mulai, tgl_selesai)
+            daftar_sponsor(request.session['user']['nama'], nama_brand, tgl_mulai, tgl_selesai)
             return redirect('list_sponsor/')
         else:
             error = "Data yang diisikan belum lengkap, silahkan lengkapi data terlebih dahulu!"
@@ -24,6 +25,5 @@ def show_daftar_sponsor(request):
     return render(request, "daftar_sponsor.html", {'pilihan_sponsor_tersedia' : pilihan_sponsor_tersedia})
 
 def show_list_sponsor(request):
-    nama_atlet = request.session['user']['nama']
-    daftar_list_sponsor = list_sponsor(nama_atlet)
+    daftar_list_sponsor = list_sponsor(request.session['user']['nama'])
     return render(request, "list_sponsor.html", {'daftar_list_sponsor' : daftar_list_sponsor})
