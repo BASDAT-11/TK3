88a98e5c1ce225b6c96614542199b1065989e9f1
dreins <davynreinhard@gmail.com>
2023-05-25 23:32:43 +0700

    refactor code

--- dashboard/views.py
+++ dashboard/views.py
@@ -19,21 +19,19 @@ def base_page(request):
 def dashboard_page(request):
     cursor = connection.cursor()
     user_logged_in = None
-    try:
-        if request.session['is_atlet'] or request.session['is_pelatih'] or request.session['is_umpire']:
-            if request.session['user']['role'] =='atlet':
-                user_logged_in = SQLprofileAtlet(request.session['user']['id'])
-            elif request.session['user']['role'] =='pelatih':
-                user_logged_in = SQLprofilePelatih(request.session['user']['id'])
-            elif request.session['user']['role'] =='umpire':
-                user_logged_in = SQLprofileUmpire(request.session['user']['id'])
-            else:
-                return HttpResponseRedirect(reverse("authentication:user_login"))
+    if request.session['is_atlet'] or request.session['is_pelatih'] or request.session['is_umpire']:
+        if request.session['user']['role'] =='atlet':
+            user_logged_in = SQLprofileAtlet(request.session['user']['id'])
+        elif request.session['user']['role'] =='pelatih':
+            user_logged_in = SQLprofilePelatih(request.session['user']['id'])
+        elif request.session['user']['role'] =='umpire':
+            user_logged_in = SQLprofileUmpire(request.session['user']['id'])
         else:
             return HttpResponseRedirect(reverse("authentication:user_login"))
-
     else:
         return HttpResponseRedirect(reverse("authentication:user_login"))
+
+
     
     status="Not Qualified"
 
@@ -51,10 +49,6 @@ def dashboard_page(request):
 
     print ("TEST")
     print(user_logged_in)
-
-
-    except KeyError:
-        return HttpResponseRedirect(reverse("authentication:main_auth"))
 
     context = {
         'user_logged_in' : user_logged_in[0],
