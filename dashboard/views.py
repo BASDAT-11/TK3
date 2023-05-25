637c77ef057ff05e45e2a4e4ffd260a2719f7a67
dreins <davynreinhard@gmail.com>
2023-05-25 22:54:41 +0700

    refactor code

--- dashboard/views.py
+++ dashboard/views.py
@@ -19,19 +19,20 @@ def base_page(request):
 def dashboard_page(request):
     cursor = connection.cursor()
     user_logged_in = None
-    if ('is_atlet' not in request.session) and ('is_pelatih' not in request.session) and ('is_umpire' not in request.session):
-        return HttpResponseRedirect(reverse("authentication:main_auth"))
-    if request.session['is_atlet'] or request.session['is_pelatih'] or request.session['is_umpire']:
-        if request.session['user']['role'] =='atlet':
-            user_logged_in = SQLprofileAtlet(request.session['user']['id'])
-        elif request.session['user']['role'] =='pelatih':
-            user_logged_in = SQLprofilePelatih(request.session['user']['id'])
-        elif request.session['user']['role'] =='umpire':
-            user_logged_in = SQLprofileUmpire(request.session['user']['id'])
+    try:
+        if request.session['is_atlet'] or request.session['is_pelatih'] or request.session['is_umpire']:
+            if request.session['user']['role'] =='atlet':
+                user_logged_in = SQLprofileAtlet(request.session['user']['id'])
+            elif request.session['user']['role'] =='pelatih':
+                user_logged_in = SQLprofilePelatih(request.session['user']['id'])
+            elif request.session['user']['role'] =='umpire':
+                user_logged_in = SQLprofileUmpire(request.session['user']['id'])
+            else:
+                return HttpResponseRedirect(reverse("authentication:user_login"))
         else:
             return HttpResponseRedirect(reverse("authentication:user_login"))
-    else:
-        return HttpResponseRedirect(reverse("authentication:user_login"))
+    except KeyError:
+        return HttpResponseRedirect(reverse("authentication:main_auth"))
     
     context = {
         'user_logged_in' : user_logged_in[0]
